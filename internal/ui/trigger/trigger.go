0c42e36a86778b9956c5a1c1bf128bedd9d64006
mr-chelyshkin <argent-keel-0z@icloud.com>
2026-02-17 19:11:41 +0400

    update

--- internal/ui/trigger/trigger.go
+++ internal/ui/trigger/trigger.go
@@ -6,3 +6,11 @@ const (
 	UserUpdate        = "user_update"
 	UserSessionUpdate = "user_session_update"
 )
+
+const (
+	Every30s = "every 30s"
+	Every1m  = "every 60s"
+	Every5m  = "every 300s"
+
+	UserSessionsRefresh = Every1m
+)
